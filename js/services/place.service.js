b16be110d57dec0541532ea4b98908e1b5380686
Yarden Naor <naoryarden@gmail.com>
2022-12-21 15:51:18 +0200

    working in placeService

--- js/services/place.service.js
+++ js/services/place.service.js
@@ -1,5 +1,14 @@
-
+import { utilsSrevice } from "./utils.service"
 
 function setPlace(cord){
 
+    const place = {
+        id:  utilsSrevice.getRandomId(),
+        name: setName()  ,
+        lat: cord.lat,
+        lng: cord.lng,
+        weather: getWeather(),
+        createdAt: Date.now,
+        updatedAt: UpdateTime()
+    }
 }
